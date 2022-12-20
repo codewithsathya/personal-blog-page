8ca7f1641add09e8d273151bd3dfeb53fcd8ed9d
Sathiya Seelan <codewithsathya@MacbookPro.local>
2022-12-20 12:07:23 +0530

    Modify siteMetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,23 +2,22 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'CodeWithSathya Blog',
+  author: 'Sathiya Seelan',
+  headerTitle: 'CodeWithSathya',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog.codewithsathya.com',
+  siteRepo: 'https://github.com/codewithsathya/personal-blog-page',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'codewithsathya@gmail.com',
+  github: 'https://github.com/codewithsathya',
+  twitter: 'https://twitter.com/codewithsathiya',
+  facebook: 'https://www.facebook.com/myselfsathya',
+  linkedin: 'https://www.linkedin.com/in/myselfsathya/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
