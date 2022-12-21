10ec86f3d68e6e726fe0ae5380a2f8e2bed9d80e
codewithsathya <codewithsathya@gmail.com>
2022-12-21 06:50:41 +0000

    Change name of the pm2 process

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "portfolio",
+      name: "personal-website",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3001",
     },
